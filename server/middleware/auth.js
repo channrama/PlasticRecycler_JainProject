5a609538d022c961dcb252267e4d922f72ef7c07
jayanthyadavhm <1si22cs074.sit.ac.in>
2024-11-14 21:44:42 +0530

    checking

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
+  //nothing
   const token = req.header('x-auth-token');
   if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
